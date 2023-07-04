64a6359997a4898a93dd31fd9c3a97f1d246c79e
Omkar Kolekar <okolekar123@gmail.com>
2023-07-04 02:35:58 +0200

    H as a time History.py
    1) H now stores the enthalpy of all the timesteps.
    2) dT/dH is now a matrix
    3) Added the plotting file postProcessing
    4) G is now based on the previous time step of the enthalpy and not the previous NRS scheme as this was the error earlier.

--- EnthalpyApproach.py
+++ EnthalpyApproach.py
@@ -1,6 +1,8 @@
 import numpy as np
 import Initial_Temperature_Dist as IT
+import postProcessor as pp
 import sys
+#import tempCodeRunnerFile as TCR
 
 #The length of the material is assumed to be 1m
 #The number of ELEMENTS decided are  10
@@ -19,7 +21,7 @@ D = 0.7970
 Ta = -0.4             # ambient temperature in Kelvin.
 #T = Ta*np.ones(11).reshape(11,1)    # The temperature array for the initial start. 	  	  #In the unit K.
 lambf = 0.25 	  	  # The lambda constant 
-
+np.set_printoptions(edgeitems=11, threshold=np.inf, linewidth=np.inf, suppress=True, formatter={'int': lambda x: str(x)})
 # Hm is considered to be zero as it is dimensionless and at the start of melting the Hm = 0 
 
 def mesh_list():
@@ -30,9 +32,14 @@ def mesh_list():
         l.append(i/10)           
     return l
 
-T = IT.Initial_Temp(ratioI,Ta,1.14,mesh_list())\
-    .Tdist_Enthalpy().copy().reshape(11,1) #The temperature array for the initial start.    #Dimensionless. 
-Hk = D*T          # The enthalpy was calculated as taking into account the above temperature array
+T = IT.Initial_Temp(ratioI,Ta,delt,mesh_list())\
+    .Tdist_Enthalpy().copy().reshape(11,1) #The temperature array for the initial start.  #Dimensionless.
+#print(f"The initial temprature distribution looks like\n {T} ") #1.14
+H = D*T          # The enthalpy was calculated as taking into account the above temperature array
+#print("Initial Enthalpy :-")
+#print(Hk)
+
+
 
 def phi(zeta):
     shape_fun=np.array([0.5*(1-zeta),0.5*(1+zeta)]) # The shape function is Linear.
@@ -48,7 +55,7 @@ def J(i):
     x2 = node_list[i+1]
     return x2-x1
 
-def Matrial_model(zeta,i,Te,Hk_1,Hk):
+def Matrial_model(zeta,i,Te,Hk_1,Het_1):
     '''Physics subroutine or material subroutine'''
 
     global delt,ratioI,n,D,Ta,lambf
@@ -59,14 +66,14 @@ def Matrial_model(zeta,i,Te,Hk_1,Hk):
         b = - Ta*phi_derivative(i)[1]*phi_derivative(i)
 
     
-    dT_dH = [[1/D if Hk[0] < 0 else 0 if Hk[0] ==  0 else 1/D, 0],
-             [0, 1/D if Hk[1] < 0 else 0 if Hk[1] ==  0 else 1/D]]
+    dT_dH = [[1/D if Hk_1[0] < 0 else 0 if Hk_1[0] ==  0 else 1/D, 0],
+             [0, 1/D if Hk_1[1] < 0 else 0 if Hk_1[1] ==  0 else 1/D]]
     
 
     M = np.matmul(phi(zeta),phi(zeta).reshape(1,2))
     N = np.matmul(phi_derivative(i),phi_derivative(i).reshape(1,2))
     F = D*(b-np.matmul(N,Te))
-    G = np.matmul(M,(Hk_1-Hk)) - delt*F
+    G = np.matmul(M,(Hk_1-Het_1)) - delt*F
     dG = M + delt*D*np.matmul(N,dT_dH)
     return G,dG
 
@@ -76,33 +83,39 @@ def Aly(p):               # here the n represents the total number of elements a
     A[p+1,1] = 1 
     return A
 
-def GaussLoop(He_1,He,Te,i):    
+def GaussLoop(He_1,Te,i,Het_1):    
     Gauss_points = [-0.577350269189626, 0.577350269189626] # Gauss points for the integration
     Ge = np.zeros([2,1])
     dGe = np.zeros([2,2])
     for zeta in Gauss_points: # zeta is the Gauss coordinate value of the single element.
                 ''' Material model below'''
-                MaterialCall = Matrial_model(zeta,i,Te,He_1,He)
+                MaterialCall = Matrial_model(zeta,i,Te,He_1,Het_1)
                 Gt = MaterialCall[0]
                 dGt = MaterialCall[1]
                 Ge = Ge + Gt
                 dGe = dGe +dGt
     return Ge,dGe
 
+
+
 def main():
-    global Hk,delt,ratioI,n,D,Ta,lambf,T
-    for t in np.arange(0,1,delt):
-         Ht= Hk.copy() # at the start we assume that the new and previous enthalpies are same
-         #Ht = new enthalpy after the newton Raphson scheme          #Hk is the previous Enthalpy      #H_1e is the elemental new enthalpy     #He is the elemental old enthalpy 
-         G = np.zeros([n,1])
-         dG = np.zeros([n,n])
-         while True:
+    global H,delt,ratioI,n,D,Ta,lambf,T
+    for t in range(10): # earlier was 0,1,delt
+        #print("The current time is {}".format(t))
+        Hk= H[:,t][:,np.newaxis].copy() #Variable used for NRS scheme
+        Ht = Hk.copy() # at the start we assume that the new and previous enthalpies are same
+        Ht_1 = Hk.copy()
+        #Ht = new enthalpy after the newton Raphson scheme          #Ht_1 is the time-1 Enthalpy      #H_1e is the elemental new enthalpy     #He is the elemental old enthalpy 
+        G = np.zeros([n,1])
+        dG = np.zeros([n,n])
+        while True:
             for i in range(n-1): #n is the number of elements in the mesh. i is the element number.
                 A = Aly(i) 
-                He = np.matmul(A.T,Hk) # Assembly matrix for element i. He is a 2x1 vector.
+                #He = np.matmul(A.T,Hk) # Assembly matrix for element i. He is a 2x1 vector.
                 He_1 = np.matmul(A.T,Ht)
+                Het_1 = np.matmul(A.T,Ht_1) 
                 Te = np.matmul(A.T,T) #Elemental T extracted from universal T 
-                [Ge,dGe] = GaussLoop(He_1,He,Te,i)
+                [Ge,dGe] = GaussLoop(He_1,Te,i,Het_1)
                 G = G + np.matmul(A,Ge)   #G is a column vector
                 dG = dG + np.matmul(np.matmul(A,dGe),A.T)    #(n,2)*(2,2)*(2,n) 
 
@@ -115,15 +128,16 @@ def main():
             '''np.set_printoptions(edgeitems=11, threshold=np.inf, linewidth=np.inf, suppress=True, formatter={'int': lambda x: str(x)})
             print(np.array2string(dG,separator = ',')) ####DELETE THIS LINE'''   
             Ht = Hk - np.matmul(np.linalg.inv(dG),G)
-            if (abs(Ht - Hk).all()<np.exp(-5)):
+            #Condition = TCR.condition(Ht,Hk)
+            diff = np.abs(Ht-Hk)
+            if np.all(diff<np.exp(-5)):
                 break #If the difference between the new and previous enthalpy is lower than tolerance, then the NRS Scheme is over.
             else:
                 Hk = Ht.copy() #Otherwise, the NRS Scheme continues to optimize the Enthalpy.
                 T = Hk/(D)
-    return Ht,T
+        H = np.column_stack((H,Ht))
+    return Ht,T,t
 
-[Ht,T] = main()
-
-print(Ht)
-print("Printing T")
-print(T)
+[Ht,T,t] = main()
+print(np.column_stack((H,T)))
+#pp.plotTemprature(T,t)
