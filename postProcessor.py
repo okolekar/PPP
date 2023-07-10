230b37d1fe0695253c614cf1b800167a2687d0b9
Omkar Kolekar <okolekar123@gmail.com>
2023-07-10 03:16:12 +0200

    Update postProcessor.py
    Added legends

--- postProcessor.py
+++ postProcessor.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
-def plotTemprature(T,t):
-    plt.plot(T)
+def plotTemprature(T0,T1,t):
+    plt.plot(T0,label='Initial Temperature Distribution')
+    plt.plot(T1, label='Final Temperature Distribution')
     plt.xlabel('Node number')
     plt.ylabel('Dimensionless Temperature')
     plt.title(f'Temperature Vs Depth at time {t}')
+    plt.legend()
     plt.show()
