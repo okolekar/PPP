3349d866213b29619007aefde2259264c8e6b02d
Omkar Kolekar <okolekar123@gmail.com>
2023-07-12 18:48:47 +0200

    Update variableUpdater.py
    Added a checker wrapper for the no of input variables

--- variableUpdater.py
+++ variableUpdater.py
@@ -1,20 +1,30 @@
 import numpy as np
 
-def Update(D,lambf,T=None,H=None):
+def check_arguments(func,*args,**kwargs):
+    def wrapper(*args,**kwargs):
+        if func.__name__ == 'Update_Crys' and len(kwargs) != 4:
+            raise ValueError("Incorrect number of input arguments to Update_Crys.")
+        elif func.__name__ == 'Update_amorphus' and len(kwargs) != 6:
+            raise ValueError("Incorrect number of input arguments to Update_amorphus.")
+        return func(*args,**kwargs)
+    return wrapper
+
+@check_arguments
+def Update_Crys(D,lambf,T=None,H=None):
     if T is not None:
         return np.where(T < 0, D*T, np.where(T == 0,D*lambf, D*T+D*lambf))
     elif H is not None:
         return np.where(H < 0, H/D, np.where(H <= lambf*D,0, (H-D*lambf)/D))
     else:
-        print("Problem in the program detected. Terminating further execution")
-
+        print("Update_Crys() failed to execute")
+        raise ValueError("Enthalpy and/or Temperature not provided. Hence, terminating further execution")
+    
+@check_arguments
 def Update_amorphus(D,lambf,Tliq,T=None,H=None,Hliq=None):
     if T is not None:
         return np.where(T <= 0, D*T, np.where(np.logical_and(T >= 0, T<= Tliq),D*lambf*T/Tliq, D*T+D*lambf))
     elif H is not None:
-        if Hliq is None:
-            print("Hliq is not provided. Value override")
-        else:
-            return np.where(H <= 0, H/D, np.where(H <= Hliq,H*Tliq/(D*Tliq+D*lambf), (H-D*lambf)/D))
+        return np.where(H <= 0, H/D, np.where(H <= Hliq,H*Tliq/(D*Tliq+D*lambf), (H-D*lambf)/D))
     else:
-        print("Problem in the program detected. Terminating further execution")
+        print("Update_amorphus() failed to execute")
+        raise ValueError("Enthalpy and/or Temperature not provided. Hence, terminating further execution")
