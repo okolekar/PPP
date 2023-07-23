0837a4afeba3628748cc04b093095c0fa93a3703
Omkar Kolekar <okolekar123@gmail.com>
2023-07-24 00:52:08 +0200

    Update postProcessor.py
    Many functionalities added to have multiple graphs

--- pymelting_library/postProcessor.py
+++ pymelting_library/postProcessor.py
@@ -1,5 +1,5 @@
+import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
 
 def plotTemprature(T0,T1,t):
     plt.plot(T0,label='Initial Temperature Distribution')
@@ -8,5 +8,35 @@ def plotTemprature(T0,T1,t):
     plt.xlabel('Node number')
     plt.ylabel('Dimensionless Temperature')
     plt.title(f'Temperature Vs Depth at time {t}')
+    plt.legend()
+    plt.show()
+
+def Simultaneous_Plotter(X,name1,Y,name2):
+    matplotlib.use('TkAgg')
+    for i in range(X.shape[1]):
+        plt.plot(X[:,i], label=f't{i}')
+    plt.xlabel('Node number')
+    plt.ylabel(f'Dimensionless {name1}')
+    plt.title(f'Variation of {name1} Vs Depth over time')
+    plt.legend()
+    plt.show(block=False)
+
+    plt.figure()
+
+    for i in range(Y.shape[1]):
+        plt.plot(Y[:,i], label=f't{i}')
+    plt.xlabel('Node number')
+    plt.ylabel(f'Dimensionless {name2}')
+    plt.title(f'Variation of {name2} Vs Depth over time')
+    plt.legend()
+    plt.show()
+
+def plotVerify(*args):
+    for i in range(0,int(len(args)),2):
+        plt.plot(args[i],label=args[i+1])
+    plt.xticks(range(len(args[0])))
+    plt.xlabel('Node number')
+    plt.ylabel('Dimensionless Temperature')
+    plt.title('Temperature Verification')
     plt.legend()
     plt.show()
