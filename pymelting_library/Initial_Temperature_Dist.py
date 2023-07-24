de167d8319db87d4ce204e239f12d21226853f5f
Omkar Kolekar <okolekar123@gmail.com>
2023-07-24 22:51:51 +0200

    Update Initial_Temperature_Dist.py
    Added comments

--- pymelting_library/Initial_Temperature_Dist.py
+++ pymelting_library/Initial_Temperature_Dist.py
@@ -1,6 +1,17 @@
+'''Topic: PPP
+
+#############################################################################################################################
+Importing the required standard libraries                                                                                 '''
+#############################################################################################################################
+
 import numpy as np
 import math as m
 import inputs as ip 
+'''
+#############################################################################################################################
+Check arguments Decorator: -
+Checks if atleast 1 argument is passed to this Class                                                                      '''
+#############################################################################################################################
 
 def check_arguments(func):
     def wrapper(self, *args,**kwargs):
@@ -8,6 +19,20 @@ def check_arguments(func):
             raise ValueError("Incorrect number of input arguments to Initial_Temp.")
         return func(self,*args,**kwargs)
     return wrapper
+
+'''
+#############################################################################################################################
+Class Initial Temp: -
+As soon as the init method is called the method calculating the Temperature Distribution is called.
+Attributes:-
+T                -> Stores the Initial Temperature distribution
+
+Tests: -
+check_T          -> Checks if the first entry in the Temperature is greater than the ambient Temperature.
+
+Methods: - 
+Tdist_Enthalpy   -> Calculates the Temperature Distribution                                                               '''
+#############################################################################################################################
 
 class Initial_Temp():
     'The class generates the initial temperature distribution'
@@ -23,15 +48,37 @@ class Initial_Temp():
             print("Temperature distribution might be invalid.")
             print("As the Temperature at node 1 is lower than the Ta.")
 
+    '''
+#############################################################################################################################
+Method Tdist_Enthalpy()
+This method has the try and except as for the Test Heat Transfer Test Case, I have to set ambient Temperature to 0 as the 
+analytical solution was built on this condition. When the Ta is set 0, the tmelt becomes 0 which is in the denominator and 
+hence we have ZeroDivisionError. This is handled by changing the formula in the except. The time in the denominator is 
+changed to delt                                                                                                           '''
+#############################################################################################################################
+
     def Tdist_Enthalpy(self,list):
         F = ip.ratioI
-        for i in range(len(self.T)):
-            if i == len(self.T)-1:
-                self.T[i] = ip.Ta #This takes care of the boundary condition
-            else:
-                self.T[i] = (F*(2*np.sqrt(ip.delt/np.pi)
-                               *np.exp(-list[i]**2/(4*ip.delt))
-                                -list[i]*m.erfc(list[i]/(2*np.sqrt(ip.delt))))
-                                  + ip.Ta)
+        try:
+            for i in range(len(self.T)):
+                if i == len(self.T)-1:
+                    self.T[i] = ip.Ta #This takes care of the boundary condition
+                else:
+                    self.T[i] = (F*(2*np.sqrt(ip.t_start/np.pi)
+                                    *np.exp(-list[i]**2/(4*ip.t_start))
+                                    -list[i]*m.erfc(list[i]/(2*np.sqrt(ip.t_start))))
+                                 + ip.Ta)
+        except ZeroDivisionError:
+            print("The Ta may be set to zero: Initial Temperature calculation changed the formula")
+            print("t_start set to delt for this iteration")
+            for i in range(len(self.T)):
+                if i == len(self.T)-1:
+                    self.T[i] = ip.Ta #This takes care of the boundary condition
+                else:
+                    self.T[i] = (F*(2*np.sqrt(ip.t_start/np.pi)
+                                    *np.exp(-list[i]**2/(4*ip.delt))
+                                    -list[i]*m.erfc(list[i]/(2*np.sqrt(ip.delt))))
+                                + ip.Ta)
+            
         self.T = self.T[:,np.newaxis]
         self.check_T(ip.Ta)
