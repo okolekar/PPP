66d580f2592aa5aefd69649e920f7a5e2342e80f
Omkar Kolekar <okolekar123@gmail.com>
2023-07-26 01:34:04 +0200

    Update FEA_solve.py
    Added comments

--- pymelting_library/FEA_solve.py
+++ pymelting_library/FEA_solve.py
@@ -2,7 +2,7 @@
 
 #############################################################################################################################
 Importing the required libraries and their functions: -
-
+=============================================================================================================================
 inputs: ->                        Script where all the inputs are defined
 postProcessor: ->                 Graphs and visualization library, 
 Heat_Transfer_verification: ->    Provides an analytical solution to verify the application Heat transfer
@@ -12,7 +12,6 @@ Elemental_Subroutine: ->          Extracts the Temperatures and Enthalpy form th
 Mesh: ->                          Generates the Mesh specific parameters 
 Check_Inputs: ->                  Decorator that performs the check to ensure the parameters are physical'''
 #############################################################################################################################
-
 import numpy as np
 import inputs as ip
 import postProcessor as pp
@@ -27,16 +26,18 @@ np.set_printoptions(edgeitems=11, threshold=np.inf, linewidth=np.inf, suppress=T
 '''
 #############################################################################################################################
 Class FEA: -
+=============================================================================================================================
 Attributes: - 
+-------------
     Mesh   ->   Instance of Mesh class stores mesh related parameters
-    Tg     ->   Global temperature vector stores the temperature distribution for each time step
-    Hg     ->   Global enthalpy vector stores the enthalpy distribution for each time step
+      Tg   ->   Global temperature vector stores the temperature distribution for each time step
+      Hg   ->   Global enthalpy vector stores the enthalpy distribution for each time step
+-----------------------------------------------------------------------------------------------------------------------------
 1) The FEA class is the starting point of the numerical scheme.
 2) The main goal of this class is to calculate Tg and Hg vectors for given duration of time using the solve method.                  
 3) The init of this class is decorated by the check inputs, which ensures that the input parameters are in 
    the admissible range.'''
 #############################################################################################################################
-
 class FEA():
     @check_inputs
     def __init__(self):
@@ -46,27 +47,28 @@ class FEA():
     '''
 #############################################################################################################################
 Method solve(): -
+=============================================================================================================================
 Variables: -
 nrs     ->     Newton Raphson Steps recorder (Breaks the NRS loop if steps are more than 9)
 t       ->     time
 E       ->     Instance of Elemental subroutine class
 Hk      ->     Enthalpy calculated in the previous time Step
-Hk_1    ->     Enthalpy calculated in the current Newton Raphson Step
+Hk+1    ->     Enthalpy calculated in the current Newton Raphson Step
 Ht      ->     Enthalpy in the previous NRS step, acts as a condition to break the NRS loop
 Tk      ->     Temperature calculated in the previous Newton Raphson Step
-Tk_1    ->     Temperature calculated in the current Newton Raphson Step
+Tk-1    ->     Temperature calculated in the current Newton Raphson Step
+-----------------------------------------------------------------------------------------------------------------------------
 The method works as follows: -
 1) Extraction of the latest temperature and enthalpy values from the global temperature Tg and enthalpy Hg vectors 
    respectively by Tk and Hk. Setting Hk_1 = Hk for the first NRS step
 2) Extracted variables are passed to the Elemental_Subroutine and Gg and dGg vectors (Residual and Tangent Stiffness Matrix
    like vectors) are obtained
 3) Application of the boundary condition and finding the new Hk_1 and Tk_1
-4) Comparing for convergence and increasing the time'''
-#############################################################################################################################
-    
+4) Comparing for convergence and increasing the time                                                                      '''
+#############################################################################################################################   
     def solve(self):
         E = Elemental_Subroutine()
-        for t in range(20):
+        for t in range(1):
             nrs = 0
             Hk = self.Hg[:,t][:,np.newaxis].copy()
             Hk_1 = Hk.copy()
@@ -78,9 +80,9 @@ The method works as follows: -
             while (nrs<10):
                 Ht = Hk_1.copy()
                 E.get_param(Hk,Hk_1,Tk_1,self.Mesh)
-
+#===========================================================================================================================#
                 # Application of the Dirichlet Boundary Condition.
-
+#===========================================================================================================================#
                 E.Gg[ip.n-1] = 0
                 E.dGg[ip.n-1] = 0
                 E.dGg[:,ip.n-1] = 0
@@ -93,10 +95,8 @@ The method works as follows: -
                     break
                 elif(nrs > 9):
                     raise ValueError('Convergence failed to achieve in 10 nrs steps')
-            
             self.Tg = np.column_stack((self.Tg,Tk_1))
             self.Hg = np.column_stack((self.Hg,Hk_1)) 
-
 
 EntlpyApp = FEA()
 EntlpyApp.solve()
@@ -106,4 +106,5 @@ Tverify = HTv(EntlpyApp.Tg[:,0],ip.n,EntlpyApp.Mesh.nl,ip.alpha).T_analytical
 #print(EntlpyApp.Hg)
 #pp.plotTemprature(EntlpyApp.Tg[:,0],EntlpyApp.Tg[:,-1],1)
 #pp.Simultaneous_Plotter(EntlpyApp.Tg,'Temperature',EntlpyApp.Hg,'Enthalpy')
-pp.plotVerify(EntlpyApp.Tg[:,0],'Initial_Temperature',EntlpyApp.Tg[:,-1],'Final Temperature from the numerical scheme',Tverify,'Analytical Temperature')
+pp.plotVerify(EntlpyApp.Tg[:,0],'Initial_Temperature',EntlpyApp.Tg[:,-1],
+              'Final Temperature from the numerical scheme',Tverify,'Analytical Temperature')
