e079fc7d6313e7794da1e00764e94aeeb30a50aa
Omkar Kolekar <okolekar123@gmail.com>
2023-07-26 01:40:03 +0200

    Update Initial_Temperature_Dist.py
    Updated comments

--- pymelting_library/Initial_Temperature_Dist.py
+++ pymelting_library/Initial_Temperature_Dist.py
@@ -1,41 +1,42 @@
 '''Topic: PPP
-
+Library Initial_Temperature_Dist
 #############################################################################################################################
-Importing the required standard libraries                                                                                 '''
+Importing the required standard libraries 
+-----------------------------------------------------------------------------------------------------------------------------
+inputs -> Script where all the inputs are defined                                                                         '''
 #############################################################################################################################
-
 import numpy as np
 import math as m
 import inputs as ip 
 '''
 #############################################################################################################################
 Check arguments Decorator: -
-Checks if atleast 1 argument is passed to this Class                                                                      '''
+-----------------------------------------------------------------------------------------------------------------------------
+Checks if atleast 1 argument is passed to this Class                                                                         
 #############################################################################################################################
-
+'''
 def check_arguments(func):
     def wrapper(self, *args,**kwargs):
         if len(args) != 1:
             raise ValueError("Incorrect number of input arguments to Initial_Temp.")
         return func(self,*args,**kwargs)
     return wrapper
-
 '''
 #############################################################################################################################
 Class Initial Temp: -
+=============================================================================================================================
 As soon as the init method is called the method calculating the Temperature Distribution is called.
 Attributes:-
+------------
 T                -> Stores the Initial Temperature distribution
-
 Tests: -
 check_T          -> Checks if the first entry in the Temperature is greater than the ambient Temperature.
-
+-----------------------------------------------------------------------------------------------------------------------------
 Methods: - 
+=============================================================================================================================
 Tdist_Enthalpy   -> Calculates the Temperature Distribution                                                               '''
 #############################################################################################################################
-
 class Initial_Temp():
-    'The class generates the initial temperature distribution'
 
     @check_arguments
     def __init__(self,list):
@@ -47,16 +48,15 @@ class Initial_Temp():
             print(self.T)
             print("Temperature distribution might be invalid.")
             print("As the Temperature at node 1 is lower than the Ta.")
-
     '''
 #############################################################################################################################
 Method Tdist_Enthalpy()
+-----------------------------------------------------------------------------------------------------------------------------
 This method has the try and except as for the Test Heat Transfer Test Case, I have to set ambient Temperature to 0 as the 
 analytical solution was built on this condition. When the Ta is set 0, the tmelt becomes 0 which is in the denominator and 
 hence we have ZeroDivisionError. This is handled by changing the formula in the except. The time in the denominator is 
 changed to delt                                                                                                           '''
 #############################################################################################################################
-
     def Tdist_Enthalpy(self,list):
         F = ip.ratioI
         try:
@@ -78,7 +78,6 @@ changed to delt                                                                 
                     self.T[i] = (F*(2*np.sqrt(ip.t_start/np.pi)
                                     *np.exp(-list[i]**2/(4*ip.delt))
                                     -list[i]*m.erfc(list[i]/(2*np.sqrt(ip.delt))))
-                                + ip.Ta)
-            
+                                + ip.Ta)        
         self.T = self.T[:,np.newaxis]
         self.check_T(ip.Ta)
