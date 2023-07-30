420872b6d8bacc97150da29f62fa97c47d01966f
Omkar Kolekar <okolekar123@gmail.com>
2023-07-30 18:05:11 +0200

    Update Crystalline_inputs.py
    Changed the intensity ratio as per the paper for the SS304L

--- pymelting_library/Crystalline_inputs.py
+++ pymelting_library/Crystalline_inputs.py
@@ -1,13 +1,14 @@
-# Crystalline_Material_Constants
-# Default Material:- Aluminium
-meltpt = 9.3*10**2              #Melting Point                        # Kelvin
-vapourpt  = 2.5*10**3           #Vapour Point                         # Kelvin
-Iref = 1.5*10**10               #Reference Intensity                  # W/m^2
-I =    1.5*10**10               #Used Intensity                       # W/m^2
-k = 2.3*10**2                   #Conductivity                         # W/m-K
-delt = 0.01                     #time step size                                                                
-n = 21                          #number of nodes                       
-c = 9.0*10**2                   #specific heat capacity               # in J/kg/K 
-rho = 2.7*10**3                 #density of the material              # kg/m**3        
-Lf = 3.6*10**5                  #latent  heat of fusion               # J/kg
-depth = 1                       #total depth to be simulated          # meters
+# Amorphous_Material_Constants
+# Default Material: - SS304L
+soliduspt = 1670                 #Solidus Point                             # Kelvin
+liquidouspt = 1727               #Liqudus Point                             # Kelvin
+vapourpt  = 3375                 #Vapour Point                              # Kelvin
+Iref = 1.5*10**10                #Reference Intensity                       # W/m^2
+I =    1.0*10**10                #Used Intensity                            # W/m^2
+k = 22                           #Conductivity                              # W/m-K 
+delt = 0.001                     #time step size                                    
+n = 51                           #number of nodes                           
+c = 700                          #specific heat capacity                    # in J/kg/K
+rho = 7200                       #density of the material                   # kg/m**3  
+Lf = 261000                      #latent  heat of fusion                    # J/kg   
+depth = 1                        #total depth to be simulated               # meters  
