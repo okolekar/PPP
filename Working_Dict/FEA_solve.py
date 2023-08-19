9c08957a39f8c4676bc353fb16bc7343874bad9a
Omkar Kolekar <okolekar123@gmail.com>
2023-08-19 02:05:12 +0200

    Update FEA_solve.py
    Added the Stefan Approach and verified if it is giving any errors.
    Not giving error but there are issues.
    1) The temperature distribution is incorrect I think.
    2) The delt updater is not yet coded

--- Working_Dict/FEA_solve.py
+++ Working_Dict/FEA_solve.py
@@ -3,13 +3,13 @@
 #############################################################################################################################
 Importing the required libraries and their functions: -
 =============================================================================================================================
-Processed_Inputs: ->              Script where all the inputs are defined
-postProcessor: ->                 Graphs and visualization library, 
-Heat_Transfer_verification: ->    Provides an analytical solution to verify the application Heat transfer
-Initial_Temperature_Dist: ->      Provides the Initial Temperature Distribution 
-variableUpdater: ->               Updates the Temperature and the Enthalpy 
-Elemental_Subroutine: ->          Extracts the Temperatures and Enthalpy form the Global vector for each element
-Mesh: ->                          Generates the Mesh specific parameters
+Processed_Inputs:           ->      Script where all the inputs are defined
+postProcessor:              ->      Graphs and visualization library
+variableUpdater:            ->      Updates the Temperature, Enthalpy, rate of melting and the melt distance 
+Heat_Transfer_verification: ->      Provides an analytical solution to verify the application Heat transfer
+Initial_Conditions:         ->      Provides the Initial Conditions for both the approaches  
+Elemental_Subroutine:       ->      Extracts the Temperatures and Enthalpy form the Global vector for each element
+Mesh:                       ->      Generates the Mesh specific parameters
 #############################################################################################################################
 '''
 import numpy as np
@@ -21,7 +21,9 @@ from Initial_Conditions import Initial_Temp as IC
 from Initial_Conditions import Stefan_Initial_Conditions as SIC
 from Elemental_Subroutine import Elemental_Subroutine
 from Mesh import Mesh
-
+#===========================================================================================================================#
+                #The below section selects the material specific updater method for the enthalpy approach
+#===========================================================================================================================# 
 if ip.mat_type   == 1:
     from variableUpdater import  Update_Crys as update
 elif ip.mat_type == 2:
@@ -30,6 +32,8 @@ else:
     raise ValueError('Incorrect input value for the mateial type.')
 
 np.set_printoptions(edgeitems=11, threshold=np.inf, linewidth=np.inf, suppress=True, formatter={'int': lambda x: str(x)})
+
+#_____________________________________________*End of Import Section*_______________________________________________________#
 '''
 #############################################################################################################################
 Class FEA: -
@@ -41,8 +45,20 @@ Attributes: -
       Hg   ->   Global enthalpy vector stores the enthalpy distribution for each time step
         *subscript g shows its a global vector
 -----------------------------------------------------------------------------------------------------------------------------
+Description and Methods: -
+--------------------------
 1) The FEA class is the starting point of the numerical scheme.
-2) The main goal of this class is to calculate Tg and Hg vectors for given duration of time using the solve method.          
+    The Attributes are selected on the basis of the Approach which is passed as an argument to the class. 
+    If approach is s, then Stefan Approach is selected and the Stefan Approach specific parameters are selected else Enthalpy  
+    Approach specific  parameters are selected.
+
+2) The two main goals of this class are achieved by two methods respectively which are as follows: - 
+   a) solve:                    -> to calculate Tg and Hg vectors for given duration of time using the solve method for the 
+      ------                       Enthalpy Method and
+
+   b) Stefan_problem_solver:    -> to calculate the phase front position in the Stefan Approach
+      ----------------------
+-----------------------------------------------------------------------------------------------------------------------------          
 #############################################################################################################################
 '''
 class FEA():
@@ -50,15 +66,16 @@ class FEA():
         if approach == None:
             self.Mesh   = Mesh()
             self.Tg     = IC(self.Mesh.nl).T                    #The dot T shows that I access the Temperature variable.
-            self.Hg     = update(T = self.Tg,H=None,run = 0)
+            self.Hg     = update(T = self.Tg,H=None,run = 0) 
         elif approach == 's':
+            self.list_s = []
             self.s      = ip.s
             self.ds_dt  = ip.ds_dt
-            self.meshl = Mesh(s = 0,nodes = ip.n,length = self.s)
-            self.meshs = Mesh(s = self.s,nodes = ip.m,length = ip.length)
-            Initial = SIC(self.meshl.nl,self.meshs.nl,self.ds_dt,self.s)
-            self.Tl     = Initial.Tl
-            self.Ts     = Initial.Ts
+            self.meshl  = Mesh(s = 0,nodes = ip.n,length = self.s)
+            self.meshs  = Mesh(s = self.s,nodes = ip.m,length = ip.length)
+            Initial     = SIC(self.meshl.nl,self.meshs.nl,self.ds_dt,self.s)
+            self.Tl     = np.array(Initial.Tl)[:, np.newaxis]
+            self.Ts     = np.array(Initial.Ts)[:, np.newaxis]
         else:
             raise TypeError("Invalide Approach. Please enter s for Stefan Approach")                                   
     '''
@@ -87,7 +104,6 @@ The method works as follows: -
 '''   
     def solve(self):
         E = Elemental_Subroutine()
-        
         End_time = (1 if ip.test_case ==1 else ip.t_end)
         for t in range(End_time):
             nrs = 0
@@ -102,7 +118,7 @@ The method works as follows: -
                 Ht = Hk_1.copy()
                 E.get_param(Hk,Hk_1,Tk_1,self.Mesh)
 #===========================================================================================================================#
-                # Application of the Dirichlet Boundary Condition at the last node.
+                            # Application of the Dirichlet Boundary Condition at the last node.
 #===========================================================================================================================#
                 E.Gg[ip.n-1] = 0
                 E.dGg[ip.n-1] = 0
@@ -119,44 +135,93 @@ The method works as follows: -
                 elif(nrs > 9):
                     raise ValueError('Convergence failed to achieve in 10 nrs steps')
             self.Tg = np.column_stack((self.Tg,Tk_1))
-            self.Hg = np.column_stack((self.Hg,Hk_1)) 
-
-
+            self.Hg = np.column_stack((self.Hg,Hk_1))
+#--------------------------------------------------------End of Solve-------------------------------------------------------#
+    '''
+#############################################################################################################################
+Method Stefan_problem_solver(): -
+=============================================================================================================================
+Variables: -
+Tl_History  ->  Stores the history of the Temperature distribution in the liquid phase
+Ts_History  ->  Stores the history of the Temperature distribution in the solid phase
+End_time    ->  Total time for which Simulation runs
+E           ->  Instance of Elemental subroutine class
+Ml,Nl,bl    ->  Liquid phase specific parameters at the previous time step
+Ms,Ns,bs    ->  phase specific parameters at the previous time step
+A,B,C       ->  Temporary variables used only for calculation purpose
+LHS, RHS    ->  Left Hand Side and Right Hand Side of the liquid and solid Temperature distribution equation
+t           ->  Time Step in the Euler Forward Time Integration Scheme
+-----------------------------------------------------------------------------------------------------------------------------
+The method works as follows: -
+1) Based on the Initial conditions M,N,b values for the previous time step are calculated for both the phases and stored 
+   in the respective variables
+2) The new melt front distance is updated and the phase change travel velocity is updated
+3) Based on the new position of the melt front the mesh is updated
+4) Based on the new mesh, new Temperature distribution is calculated in both the phases
+5) Process is repeated untill the end time is achieved                                                                      
+#############################################################################################################################
+'''   
     def Stefan_problem_solver(self):
+        Tl_History = self.Tl.copy()
+        Ts_History = self.Ts.copy()
+        self.list_s.append(self.s)
         E = Elemental_Subroutine('s')
         E.get_global_Stefan_param(self.meshl,self.meshs,self.ds_dt)
-        Ml = E.Mlg
-        Nl = E.Nlg
-        bl = E.blg
-
-        Ms = E.Msg
-        Ns = E.Nsg
-        bs = E.bsg
-
-        self.s = self.s+(ip.delt/ip.lambf)*(self.Tl[-2]/(self.meshl.J*2)+self.Ts[1]/(self.meshs.J*2))
-        self.meshl = Mesh(s = 0,nodes = ip.n,length = self.s)
-        self.meshs = Mesh(s = self.s,nodes = ip.m,length = ip.length)
-        self.ds_dt = vu.ds_dt(self.meshl.h,self.meshs.h,self.Tl[-2],self.Ts[1])
-        E.get_global_Stefan_param(self.meshl,self.meshs,self.ds_dt)
-#there is a problem below. where np.eye is 2*2 matrix and the others are about 21*21 fix it
-        self.Tl = np.linalg.inv(np.eye(2)+0.5*ip.delt*np.linalg.inv(E.Mlg)*E.Nlg)\
-                  *((np.eye(2)-0.5*ip.delt*np.linalg.inv(Ml)*Nl)*self.Tl+\
-                    ip.delt*(0.5*np.linalg.inv(E.Mlg)+0.5*np.linalg.inv(Ml))*bl)
-        
-        self.Ts = np.linalg.inv(np.eye(2)+0.5*ip.delt*np.linalg.inv(E.Msg)*E.Nsg)\
-                  *((np.eye(2)-0.5*ip.delt*np.linalg.inv(Ms)*Ns)*self.Ts+\
-                    ip.delt*(0.5*np.linalg.inv(E.Msg)*self.bsg+0.5*np.linalg.inv(Ms)*bs))
-
-#EntlpyApp = FEA()
-
-#EntlpyApp.solve()
+        for t in range(10):       #ip.t_end
+            Ml = E.Mlg
+            Nl = E.Nlg
+            bl = E.blg
+            Ms = E.Msg
+            Ns = E.Nsg
+            bs = E.bsg
+            self.s = vu.s_update(self.s,self.Tl[-2],self.Ts[1], self.meshl.J,self.meshs.J)
+            self.ds_dt = vu.ds_dt(self.meshl.h,self.meshs.h,self.Tl[-2].item(),self.Ts[1].item())
+            self.meshl = Mesh(s = 0,nodes = ip.n,length = self.s)
+            self.meshs = Mesh(s = self.s,nodes = ip.m,length = ip.length)
+            E.get_global_Stefan_param(self.meshl,self.meshs,self.ds_dt)
+#===========================================================================================================================#
+                                    #Application of the Dirichlet boundary condition
+#===========================================================================================================================#
+            E.Mlg[-1,-1] = 1
+            E.Nlg[-1, :] = 0
+            E.Nlg[:, -1] = 0
+            E.Nlg[-1,-1] = 1
+#---------------------------------------------------------------------------------------------------------------------------#
+            A = np.eye(ip.n)-0.5*ip.delt*np.matmul(np.linalg.inv(Ml),Nl)
+            B = np.matmul(A,self.Tl)
+            C = ip.delt*np.matmul((0.5*np.linalg.inv(E.Mlg)+0.5*np.linalg.inv(Ml)),bl)
+            RHS = B + C
+            A = np.matmul(np.linalg.inv(E.Mlg),E.Nlg)
+            LHS = np.linalg.inv(np.eye(ip.n)+0.5*ip.delt*A)
+            self.Tl = np.matmul(LHS,RHS)
+            self.Tl[-1] = 0
+            Tl_History = np.column_stack((Tl_History,self.Tl))
+            A = np.eye(ip.m)-0.5*ip.delt*np.matmul(np.linalg.inv(Ms),Ns)
+            B = ip.delt*(0.5*np.matmul(np.linalg.inv(E.Msg),E.bsg)+0.5*np.matmul(np.linalg.inv(Ms),bs))
+            LHS = np.eye(ip.m)+0.5*ip.delt*np.matmul(np.linalg.inv(E.Msg),E.Nsg)
+            RHS = np.matmul(A,self.Ts)+B
+            self.Ts = np.matmul(np.linalg.inv(LHS),RHS)
+            self.Ts[0] = 0
+            Ts_History = np.column_stack((Ts_History,self.Ts))
+            self.list_s.append(self.s)
+        pp.Simultaneous_Plotter(Tl_History,'Liquid Temperature',Ts_History,'Solid Temperature')
+#------------------------------------------------End of Stefan_problem_solver-----------------------------------------------#
+'''
+=============================================================================================================================
+--------------------------------------------------------End of Class---------------------------------------------------------
+=============================================================================================================================
+'''
+print("Starting the Enthalpy Approach")
+EntlpyApp = FEA()
+EntlpyApp.solve()
+print("Starting the Stefan Approach")
 Stefan = FEA(approach = 's')
 Stefan.Stefan_problem_solver()
-pp.plot(Stefan.Tl,Stefan.Tl)
-'''if ip.test_case == 1:
+pp.plotVerify(Stefan.list_s,"melt surface dist")
+if ip.test_case == 1:
     Tverify = HTv(EntlpyApp.Tg[:,1],ip.n,EntlpyApp.Mesh.nl,ip.alpha).T_analytical
     pp.plotVerify(EntlpyApp.Tg[:,0],'Initial_Temperature',EntlpyApp.Tg[:,-1],
               'Final Temperature from the numerical scheme',Tverify,'Analytical Temperature')
 else:
     pp.plotTemprature(EntlpyApp.Tg[1:,0],EntlpyApp.Tg[1:,-1],1)
-    #pp.Simultaneous_Plotter(EntlpyApp.Tg,'Temperature',EntlpyApp.Hg,'Enthalpy')'''
+    #pp.Simultaneous_Plotter(EntlpyApp.Tg,'Temperature',EntlpyApp.Hg,'Enthalpy')
